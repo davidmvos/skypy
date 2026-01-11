c040995c110480e6857367601ab31ac685cb0dd0
FuchsCoder <69680700+FuchsCrafter@users.noreply.github.com>
2026-01-11 20:03:33 +0100

    Blank items module

---
+++ skypy/items.py
@@ -0,0 +1,51 @@
+"""
+
+FORMATS: 
+
+{
+    "material": "DIAMOND_SWORD",
+    "name": "Aspect of the End",
+    "category": "SWORD",
+    "tier": "RARE",
+    "stats": {
+    "DAMAGE": 100,
+    "STRENGTH": 100
+    },
+    "npc_sell_price": 56000,
+    "gemstone_slots": [
+    {
+        "slot_type": "SAPPHIRE"
+    }
+    ],
+    "can_have_power_scroll": true,
+    "museum_data": {
+    "donation_xp": 4,
+    "type": "WEAPONS",
+    "parent": {
+        "ASPECT_OF_THE_END": "ASPECT_OF_THE_VOID"
+    },
+    "mapped_item_ids": [],
+    "game_stage": "INTERMEDIATE"
+    },
+    "id": "ASPECT_OF_THE_END"
+},
+{
+    "material": "SKULL_ITEM",
+    "durability": 3,
+    "skin": {
+        "value": "ewogICJ0aW1lc3RhbXAiIDogMTYxNjg2NzgzMzE5NywKICAicHJvZmlsZUlkIiA6ICI1NjY3NWIyMjMyZjA0ZWUwODkxNzllOWM5MjA2Y2ZlOCIsCiAgInByb2ZpbGVOYW1lIiA6ICJUaGVJbmRyYSIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lY2VkZGMyM2Y5ZDk2YmFhYTBkMmQ3YjllYzE4MGNkN2JlYTU0NDdkMzljNDI1Y2E5ZTQ0ZDg4MDhlYTExZWEwIiwKICAgICAgIm1ldGFkYXRhIiA6IHsKICAgICAgICAibW9kZWwiIDogInNsaW0iCiAgICAgIH0KICAgIH0KICB9Cn0=",
+        "signature": "HentEgHeiCpODujGfPM4aXa4gKYLRBMgKIf0JrKINNs666RwwLCReHexC3zNcNrDevaqD1grxGHeGIDm2jcWCKrayDW2TBlo6jogzaxRYkbvFHwh2xvor00TYF9/AFMQRsm7eHqya90kogERg09NMlyTq9vnIgpflp6YYy04Qy5yecREJj42hWVkULVtfDik7QCG1IV66qVLf8FzB7vm5VUz6z9qWKYTHoZ+8P8x8fPC4oobtOuzphUeZ3EaoS3MCuJFkftxqComT30qdYF5kAd6ikyktsn1fwGWpbaLZKTR8mCjyr6/BtDXGws+DUasT8zP1kQVciKCXJOGstoH5nqwD09740QDUVerD3DqrTwylyJjGtxUHlhr5wTwezeNsQIOpi9VIZUZCqhhlsmrKjkElGSb9EExRHxVjJLFocui0sfiCUamOlE+vxFnxcfq4L7DBt59OXGNUtchVm6x60KsDxz5RicwRCLZLMfLCVEFMNcLJwMkW+zU7HWwmao2AWyvQuZpU091rTiypmYRnVJyj32cHYlUTzp9akluf9mS/9JqAY5T+dWQR0GghzeoruJ2f4/VOL9dCCTHkGO1Rt3vnhRLrdXs2eE8yw1KFUx5EnaoyYwM7d2BQSKT4i4CRuhMGVdmbzlenALMFgZeN5AzWT26RQT3Jwdh8s19PP0="
+    },
+    "name": "Easter Egg Minion Skin",
+    "category": "COSMETIC",
+    "museum": true,
+    "id": "EASTER_EGG_PERSONALITY"
+}
+
+URL: https://api.hypixel.net/v2/resources/skyblock/items
+
+"""
+
+
+class Item(NotImplemented):
+    raise NotImplementedError()
